4c1cb2c812c2c561b1dc63a8338a72eb8738220c
MessiTetas69 <egalindo@insgabrielamistral.cat>
2025-02-20 13:04:16 +0100

    2a modificació

--- eloyGitHub/src/eloyGitHub/Main.java
+++ eloyGitHub/src/eloyGitHub/Main.java
@@ -3,5 +3,7 @@ package eloyGitHub;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("1a modificació Git");
+
+		System.out.println("2a modificació GitHub");
 	}
 }
